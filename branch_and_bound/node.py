08d26ead38b00832e43462664e17f5dbe2fcc276
Santiago Pujol <santiagopujol7@gmail.com>
2024-06-11 10:01:27 +0100

    Added branch and bound

---
+++ branch_and_bound/node.py
@@ -0,0 +1,16 @@
+from collections import namedtuple
+
+Item = namedtuple("Item", ['index', 'value', 'weight'])
+
+class Node:
+    def __init__(self, index, taken, value, room):
+        self.index = index
+        self.taken = taken
+        self.value = value
+        self.room = room
+
+    def estimate(self, items):
+        bound = 0
+        for i in range(self.index, len(items)):
+            bound += items[i].value
+        return self.value + bound
